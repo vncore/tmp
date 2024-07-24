20e898063e71be25315a9807b532fd9925b0216a
vncore <giaiphap247.com@gmail.com>
2024-07-25 05:19:51 +0700

    no message

--- src/Config/vncore.php
+++ src/Config/vncore.php
@@ -11,7 +11,7 @@ return [
     'api_link'         => env('VNCORE_API_LINK', 'https://api.vncore.org/v1'),
     'ecommerce_mode'   => env('VNCORE_ECOMMERCE_MODE', 1),
     'search_mode'      => env('VNCORE_SEARCH_MODE', 'PRODUCT'), //PRODUCT,NEWS,CMS
-    'VNCORE_ACTIVE'    => env('VNCORE_ACTIVE', 1), // 1: active, 0: deactive - prevent load vencore package
+    'VNCORE_ACTIVE'    => env('VNCORE_ACTIVE', 0), // 1: active, 0: deactive - prevent load vencore package
     'VNCORE_DB_PREFIX' => env('VNCORE_DB_PREFIX', 'vncore_'), //Cannot change after install vncore
     'VNCORE_DB_CONNECTION' => env('VNCORE_DB_CONNECTION', env('DB_CONNECTION', 'mysql')), 
     'VNCORE_ADMIN_PREFIX' => env('VNCORE_ADMIN_PREFIX', 'vncore_admin'), //Prefix url admin, ex: domain.com/vncore_admin
